922d12d9526818f8dd630f22a12cd196e95e2dd1
abdullahsultan4026 <abdullahsultan4026@gmail.com>
2026-03-31 02:23:02 -0700

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <>
+      <div id="main">
+        <div id="para1"></div>
+        <div id="para2"></div>
+        <div id="para3"></div>
+      </div>
+      <h1>Flag of Ireland</h1>
+    </>
+  );
+}
